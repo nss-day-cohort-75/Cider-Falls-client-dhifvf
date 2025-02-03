47b2ee7cf728da8e2b10ca7ebdf51b6a918d9643
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-02-03 14:39:00 -0600

    first stretch goal met

--- scripts/Guests.js
+++ scripts/Guests.js
@@ -1,11 +1,25 @@
 import { getGuests } from "./database.js"
+import { getAreas } from './database.js';
+
 const guests = getGuests()
+
+// Function to get area for a given guest
+export const getAreaForGuest = (locationId) => {
+    const areas = getAreas(); // Get all areas (array)
+    let guestArea = ''
+    for (let area of areas) {
+        if (area.id === locationId) {
+            guestArea += area.name
+        }
+    }
+    return guestArea
+}
 
 export const guestList = () => {
     let guestHTML = ''
 
     for (const guest of guests) {
-        guestHTML += `<li>${guest.name}</li>`
+        guestHTML += `<li>${guest.name} (${getAreaForGuest(guest.locationId)})</li>`
     }
 
     return guestHTML
