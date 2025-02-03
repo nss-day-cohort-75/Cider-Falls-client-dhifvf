c28f539c20a7780967b99b3f2324866ac5970647
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-02-03 14:18:53 -0600

    finished code

--- scripts/Services.js
+++ scripts/Services.js
@@ -8,10 +8,30 @@ export const getServicesForArea = (areaId) => {
     for (let service of services) {
         // Check if the service is available in this area
         if (service.areaId.includes(areaId)) {
-            areaServicesHTML += `<li>${service.name}</li>`
+            areaServicesHTML += `<li data-type="service">${service.name}</li>`
         }
     }
     areaServicesHTML += ''
 
     return areaServicesHTML
 }
+
+document.addEventListener(
+    "click",
+    (clickEvt) => {
+        const itemClicked = clickEvt.target
+            if (itemClicked.dataset.type === "service") {
+                const serviceName = itemClicked.dataset.name
+                const newArray = []
+                const services = getServices()
+                for (const service of services) {
+                    for (element of service.areaId) {
+                        if (element === areaList.id) {
+                            newArray.push(element)
+                        }
+                    }
+                }
+                window.alert(`${serviceName} is available in ${area.name}`)
+            }
+    }
+)
